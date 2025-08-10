376a2e01506d56d39667992a1ef6ca09da55c7d0
Dann7i <hermawanwildan78@gmail.com>
2025-08-10 19:47:40 +0700

    Auto commit update 2025-08-10 19:47:40

--- auto_commit.sh
+++ auto_commit.sh
@@ -5,3 +5,6 @@ cd /data/data/com.termux/files/home/javascript
 git add .
 git commit -m "Auto commit update $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin master
+
+# Kirim notifikasi kalau selesai
+termux-notification --title "Git Auto Commit" --content "Commit & Push berhasil pada $(date '+%Y-%m-%d %H:%M:%S')" --priority high
